06a52c4dd81e35d3f36e51928b84e36f6e319ff0
Gustav <sir.gustav.the.coder@gmail.com>
2014-07-18 19:52:15 +0200

    forgot file

---
+++ core/src/com/madeso/platformer/Platformer.java
@@ -0,0 +1,34 @@
+package com.madeso.platformer;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
+
+public class Platformer extends Game {
+    public SpriteBatch batch;
+    public BitmapFont font;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+        font = new BitmapFont();
+        this.setScreen(new GameScreen(this));
+	}
+
+	@Override
+    public void render() {
+        super.render(); //important!
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+    }
+}
